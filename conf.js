e77a734dd8f1d3c520c0f2e3329644b1e995bac4
Vitalii_Priyma <vitalii_priyma@epam.com>
2019-08-15 21:05:47 +0300

    clean folders

--- conf.js
+++ conf.js
@@ -10,27 +10,46 @@ exports.config = {
     baseUrl: 'https://www.google.com/',
     searchWord: 'Automation'
   },
+
+  beforeLaunch() {
+    //clean folders
+    var fs = require('fs-extra');
+    fs.emptyDir('screenshots/');
+    fs.emptyDir('jasmine-result/');
+    fs.emptyDir('jasmine-report/');
+    fs.emptyDir('allure-results/');
+    fs.emptyDir('allure-reports/');
+  },
+
   onPrepare() {
-    //browser section
+    //browser config section
     browser.ignoreSynchronization = true;
     browser.driver.manage().window().maximize();
     
-    //typescript section
+    //typescript config section
     require('ts-node').register({
     project: require('path').join(__dirname, './tsconfig.json') // Relative path of tsconfig.json file 
     });
-
-    //allure section
-    var AllureReporter = require('jasmine-allure-reporter');
-    jasmine.getEnv().addReporter(new AllureReporter({
-      resultsDir: 'allure-results'
-    }));
 
     //jasmine-spec-reporter
     var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
     jasmine.getEnv().addReporter(new SpecReporter({ displayStacktrace: 'all' }));
 
-    //jasmineReporters
+    //allure-report config section
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+    jasmine.getEnv().afterEach(function (done) {
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
+
+    //jasmine-reporters
     var jasmineReporters = require('jasmine-reporters');
     jasmine.getEnv().addReporter(
       new jasmineReporters.JUnitXmlReporter({
@@ -39,7 +58,25 @@ exports.config = {
         filePrefix: 'xmlresults'
       })
     );
+
+    //protractor-html-reporter-2 for screenshots
+    var fs = require('fs-extra');
+    jasmine.getEnv().addReporter({
+      specDone: function (result) {
+        if (result.status == 'failed') {
+          browser.getCapabilities().then(function (caps) {
+            var browserName = caps.get('browserName');
+            browser.takeScreenshot().then(function (png) {
+              var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName + '.png');
+              stream.write(new Buffer(png, 'base64'));
+              stream.end();
+            });
+          });
+        }
+      }
+    });
   },
+
   onComplete() {
     //HTMLReport called once tests are finished
     var browserName, browserVersion;
@@ -67,3 +104,5 @@ exports.config = {
     });
   }
 };
+
+
